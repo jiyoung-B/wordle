f9647b62ae8e4518e185ec860214e48ba6fbdc69
jiyoung-B <catgirl0313@naver.com>
2023-07-03 19:04:40 +0900

    fastapi

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id:int
+    content:str
+
+app = FastAPI()
+items = ['맥북', '애플워치', '아이폰', '에어팟']
+
+
+@app.get("/items")
+def read_items():
+    return items
+  
+@app.get("/items/{id}")
+def read_id_item(id):
+    return items[int(id)]
+  
+@app.get("/items")
+def read_id_item(skip:int=0, limit:int=10):
+    return items[skip:skip+limit]
+  
+  
+@app.get("/hello")
+def sayHello():
+    return {"message": "안녕하세요"}
+
+@app.get("/")
+def sayWelcome():
+    return {"message": "환영합니다"}
+  
+  
+@app.post("/")
+def post_item(item:Item):
+  items.append(item.content)
+  return "성공했습니다."
